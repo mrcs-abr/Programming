0af5e8f341e4340876502602477120afd8d75591
mrcs-abr <marcos.br258@gmail.com>
2024-08-15 18:03:38 -0300

    Added solutions

---
+++ Beecrowd/C/bee2344.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+
+  scanf("%d", &n);
+
+  if (n == 0)
+    printf("E\n");
+  else if (1 <= n && n <= 35)
+    printf("D\n");
+  else if (36 <= n && n <= 60)
+    printf("C\n");
+  else if (61 <= n && n <= 85)
+    printf("B\n");
+  else
+    printf("A\n");
+
+  return 0;
+}
