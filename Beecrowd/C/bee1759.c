3f1478f5be894917299ea07593f7bdd3f4bdc48e
mrcs-abr <marcos.br258@gmail.com>
2024-08-28 22:16:40 -0300

    Added solutions

---
+++ Beecrowd/C/bee1759.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+
+  scanf("%d", &n);
+
+  for (int i = 0; i < n; i++) {
+    printf("Ho");
+
+    if (i != n - 1)
+      printf(" ");
+    else
+      printf("!\n");
+  }
+
+  return 0;
+}
