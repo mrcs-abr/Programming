e3583f920106346f5d9571b0b6daa66a04c9c793
mrcs-abr <marcos.br258@gmail.com>
2024-09-02 22:43:39 -0300

    Added solutions

---
+++ Beecrowd/C/bee1924.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  char course[256];
+
+  scanf("%d", &n);
+
+  for (int i = 0; i < n; i++) {
+    scanf("%s", course);
+  }
+
+  printf("Ciencia da Computacao\n");
+
+  return 0;
+}
