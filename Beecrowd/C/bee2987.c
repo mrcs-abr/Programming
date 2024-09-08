c016b0d98f894e0c5b69f136d6828350e7f78158
mrcs-abr <marcos.br258@gmail.com>
2024-09-07 22:29:32 -0300

    Added solutions

---
+++ Beecrowd/C/bee2987.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  char letter;
+  int index;
+  char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  scanf("%c", &letter);
+
+  for (int i = 0; i < 23; i++) {
+    if (letter == alphabet[i])
+      index = i + 1;
+  }
+
+  printf("%d\n", index);
+
+  return 0;
+}
