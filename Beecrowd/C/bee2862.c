372d84531100db462e4280ca065ae01637abd948
mrcs-abr <marcos.br258@gmail.com>
2024-09-08 23:27:48 -0300

    Added solutions

---
+++ Beecrowd/C/bee2862.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int c, n;
+
+  scanf("%d", &c);
+
+  for (int i = 0; i < c; i++) {
+    scanf("%d", &n);
+
+    if (n > 8000)
+      printf("Mais de 8000\n");
+    else
+      printf("Inseto\n");
+  }
+
+  return 0;
+}
